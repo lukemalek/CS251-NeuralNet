d220641afc68def5ec0ce77973e9e1dd4b76d6ca
Luke Malek <malek1@stolaf.edu>
2019-05-06 13:54:11 -0500

    good demo for class

--- englishLearner.cpp
+++ englishLearner.cpp
@@ -3,40 +3,59 @@
 int main()
 {
     srand(time(NULL));
-    const vector<int> dimentions = {56*40, 60,20, 2};
+    const vector<int> dimentions = {1200, 60, 20, 2};
     int firstLayer = dimentions[0];
 
     ifstream english("frankenstein.txt");
     ifstream gibberish("gibberish.txt");
 
     vector<string> engData, gibData;
-    for ( int i = 0; i<7720; i++)
+    for (int i = 0; i < 7720; i++)
     {
-        string line; 
-        getline(english,line);
+        string line;
+        getline(english, line);
         engData.push_back(line);
     }
-    for(int i = 0; i<6558; i++)
+    for (int i = 0; i < 6558; i++)
     {
-        string line; 
-        getline(gibberish,line);
+        string line;
+        getline(gibberish, line);
         gibData.push_back(line);
     }
 
     Network learner("english.net");
-    
-    cout << "enter a phrase : ";
-    string answer;
-    getline(cin, answer);
-    learner.setInputLayer(formToInput(answer, learner.getInputSize(),false));
-    learner.evaluate();
-    if(learner.getOutput(0) > learner.getOutput(1))
-        cout << "I think that is english!" << endl;
-    else
-        cout << "I think that is gibberish!" << endl;
-    cout <<learner.getOutput(0) << ' '<< learner.getOutput(1) << endl;
-    /*
+    int correct=0,attempted=0;
+    bool keepGo = true;
+    while (keepGo)
+    {
+        
+        cout << "Enter a phrase : ";
+        string answer;
+        getline(cin, answer);
+        
+        if(answer[0] == 0)
+        {
+            keepGo = false;
+            break;
+        }
+        attempted++;
+        learner.setInputLayer(formToInput(answer, learner.getInputSize(), false));
+        learner.evaluate();
+        if (learner.getOutput(0) > learner.getOutput(1))
+            cout << "I think that is english! Was I right? (y/n)" << endl;
+        else
+            cout << "I think that is gibberish! Was I right? (y/n)" << endl;
+        cout << learner.getOutput(0) << ' ' << learner.getOutput(1) << endl;
+        string ans;
+        getline(cin, ans);
+        if(ans[0] == 'y' || ans[0] == 'Y')
+        {
+            correct++;
+        }
+    }
+    cout << "I guessed " << correct << " out of " << attempted << " correctly! (" << ((float)correct)/attempted << ")\n";
 
+    /*
     int subsetSize = 10, rate = 10;
 
     for(int i = 0; i< 500; i++)
@@ -49,14 +68,14 @@ int main()
             if(rnum() > 0.5)
             {
                 int spot = (int)(7720 * rnum());
-                input = formToInput(engData[spot],firstLayer, true);
+                input = formToInput(engData[spot],firstLayer, false);
                 wanted[0] =1;
                 wanted[1] = 0;
             }
             else
             {
                 int spot = (int)(5334 * rnum());
-                input = formToInput(gibData[spot],firstLayer,true);
+                input = formToInput(gibData[spot],firstLayer,false);
                 wanted[0] = 0;
                 wanted[1] = 1;
             }
@@ -73,7 +92,4 @@ int main()
     }
     learner.toFile("englishUpper.net");
     */
-
-
-    
 }
